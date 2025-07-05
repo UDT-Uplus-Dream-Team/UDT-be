4af4a7f094b0b867d08e2965ab3a29a3b45fd829
dudxo <father8475@naver.com>
2025-07-05 16:44:10 +0900

    init project

---
+++ src/main/java/com/example/udtbe/UdtBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.udtbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdtBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdtBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udtbe/UdtBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udtbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdtBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
