3c997575df2e8df4ec02561d45d390f4690dc70f
dudxo <father8475@naver.com>
2025-07-06 01:32:37 +0900

    UDT-23 feat: Project Global Exception Init

---
+++ src/main/java/com/example/udtbe/global/exception/ErrorCode.java
@@ -0,0 +1,11 @@
+package com.example.udtbe.global.exception;
+
+import org.springframework.http.HttpStatus;
+
+public interface ErrorCode {
+	String name();
+
+	HttpStatus getHttpStatus();
+
+	String getMessage();
+}
---
+++ src/main/java/com/example/udtbe/global/exception/ErrorResponse.java
@@ -0,0 +1,8 @@
+package com.example.udtbe.global.exception;
+
+public record ErrorResponse(
+
+	String message,
+	String code
+) {
+}
