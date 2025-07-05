cef0d59f553e62a809feeb75ca77670b282317c4
dudxo <father8475@naver.com>
2025-07-06 01:31:19 +0900

    UDT-23 feat: Project Golbal Config Init

---
+++ src/main/java/com/example/udtbe/global/config/SecurityConfig.java
@@ -0,0 +1,63 @@
+package com.example.udtbe.global.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+			.csrf((auth) -> auth.disable())
+			.formLogin((auth) -> auth.disable())
+			.httpBasic((auth) -> auth.disable())
+			.sessionManagement(
+				(session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			);
+
+		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return web -> web.ignoring()
+			.requestMatchers(
+				"/error", "/favicon.ico", "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html"
+			);
+	}
+
+	// Spring Security cors Bean 등록
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+
+		configuration.setAllowedOrigins(
+			Arrays.asList(
+				"http://localhost:3000", "http://localhost:8080", "/ws/**"
+			));
+		configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		configuration.setAllowedHeaders(Arrays.asList("*"));
+		configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+		configuration.setAllowCredentials(true);
+		configuration.setMaxAge(3000L);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
+	}
+}
