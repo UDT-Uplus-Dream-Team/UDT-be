6bde98a0d2d3e55e95e5da2f904f50d822e87413
dudxo <father8475@naver.com>
2025-07-07 19:36:20 +0900

    UDT-19 feat: Auth 관련 쿼리 메서드 및 회원가입 로직

--- src/main/java/com/example/udtbe/domain/auth/service/AuthService.java
+++ src/main/java/com/example/udtbe/domain/auth/service/AuthService.java
@@ -1,6 +1,13 @@
 package com.example.udtbe.domain.auth.service;
 
+import com.example.udtbe.domain.member.entity.Member;
+import com.example.udtbe.domain.member.entity.enums.Gender;
+import com.example.udtbe.domain.member.entity.enums.Role;
+import com.example.udtbe.global.security.dto.Oauth2Response;
+import com.example.udtbe.global.token.service.TokenStore;
+import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,4 +15,24 @@ import org.springframework.stereotype.Service;
 public class AuthService {
 
     private final AuthQuery authQuery;
+    @Qualifier("redisTokenStore")
+    private final TokenStore tokenStore;
+
+    public Member saveOrUpdate(Oauth2Response oauth2Response) {
+        Member member = authQuery.getOptionalMemberByEmail(oauth2Response.getEmail())
+                .map(m -> {
+                    tokenStore.deleteRefreshTokenIfExists(m);
+                    tokenStore.deleteOauthAccessTokenIfExists(m);
+                    tokenStore.saveOauth2AccessToken(oauth2Response, m);
+                    return m;
+                })
+                .orElseGet(() -> createMemberFromOauth2Response(oauth2Response));
+
+        return authQuery.save(member);
+    }
+
+    private Member createMemberFromOauth2Response(Oauth2Response oauth2Response) {
+        return Member.of(oauth2Response.getEmail(), oauth2Response.getName(), Role.ROLE_GUEST,
+                oauth2Response.getProfileImageUrl(), Gender.MAN, LocalDateTime.now(), false);
+    }
 }
--- src/main/java/com/example/udtbe/domain/member/repository/MemberRepository.java
+++ src/main/java/com/example/udtbe/domain/member/repository/MemberRepository.java
@@ -1,8 +1,10 @@
 package com.example.udtbe.domain.member.repository;
 
 import com.example.udtbe.domain.member.entity.Member;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByEmail(String email);
 }
