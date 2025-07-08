a517c55f5bfb1a54fe36a037e8c1d21b9ab2b0b0
dudxo <father8475@naver.com>
2025-07-08 11:11:24 +0900

    UDT-19 fix: Security Authorization Error HttpStatus Code

--- src/main/java/com/example/udtbe/global/security/exception/SecurityErrorCode.java
+++ src/main/java/com/example/udtbe/global/security/exception/SecurityErrorCode.java
@@ -9,11 +9,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SecurityErrorCode implements ErrorCode {
 
-    UNAUTHORIZED_USER(HttpStatus.UNAUTHORIZED, "비인가 사용자 요청입니다."),
-    FORBIDDEN_USER(HttpStatus.UNAUTHORIZED, "USER 권한이 필요합니다."),
-    FORBIDDEN_GUEST(HttpStatus.UNAUTHORIZED, "GUEST 권한이 필요합니다."),
-    FORBIDDEN_ADMIN(HttpStatus.UNAUTHORIZED, "ADMIN 권한이 필요합니다."),
-    FORBIDDEN_MISMATCH(HttpStatus.UNAUTHORIZED, "어떤 권한도 매치되지 않습니다.");
+    UNAUTHORIZED_USER(HttpStatus.FORBIDDEN, "비인가 사용자 요청입니다."),
+    FORBIDDEN_USER(HttpStatus.FORBIDDEN, "USER 권한이 필요합니다."),
+    FORBIDDEN_GUEST(HttpStatus.FORBIDDEN, "GUEST 권한이 필요합니다."),
+    FORBIDDEN_ADMIN(HttpStatus.FORBIDDEN, "ADMIN 권한이 필요합니다."),
+    FORBIDDEN_MISMATCH(HttpStatus.FORBIDDEN, "어떤 권한도 매치되지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
