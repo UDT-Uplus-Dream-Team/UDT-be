65adec1a8493bee8d44640b4da5240a385b5641b
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-07-12 04:57:59 +0900

    UDT-21 fix: Using Projection

--- src/main/java/com/example/udtbe/domain/content/repository/ContentRepositoryImpl.java
+++ src/main/java/com/example/udtbe/domain/content/repository/ContentRepositoryImpl.java
@@ -3,9 +3,8 @@ package com.example.udtbe.domain.content.repository;
 import static com.example.udtbe.domain.content.entity.QContent.content;
 
 import com.example.udtbe.domain.admin.dto.common.ContentDTO;
-import com.example.udtbe.domain.content.entity.Content;
-import com.example.udtbe.domain.content.entity.QContent;
 import com.example.udtbe.global.dto.CursorPageResponse;
+import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
@@ -22,33 +21,32 @@ public class ContentRepositoryImpl implements ContentRepositoryCustom {
     public CursorPageResponse<ContentDTO> findContentsAdminByCursor(Long cursor, int size) {
 
         BooleanExpression cursorCondition = (cursor != null) ? content.id.lt(cursor) : null;
-        BooleanExpression notDeletedCond = content.isDeleted.eq(false);
+        BooleanExpression notDeleted = content.isDeleted.eq(false);
 
-        List<Content> contents = queryFactory
-                .select(new QContent(content))
+        List<ContentDTO> dtos = queryFactory
+                .select(Projections.constructor(
+                        ContentDTO.class,
+                        content.id,
+                        content.title,
+                        content.posterUrl,
+                        content.openDate,
+                        content.rating
+                ))
                 .from(content)
-                .where(cursorCondition, notDeletedCond)
+                .where(cursorCondition, notDeleted)
                 .orderBy(content.id.desc())
                 .limit(size + 1)
                 .fetch();
 
-        boolean hasNext = contents.size() > size;
-
+        boolean hasNext = dtos.size() > size;
         if (hasNext) {
-            contents.remove(contents.size() - 1);
+            dtos.remove(dtos.size() - 1);
         }
 
-        String nextCursor = hasNext ? String.valueOf(contents.get(contents.size() - 1).getId()) : null;
+        String nextCursor = hasNext
+                ? String.valueOf(dtos.get(dtos.size() - 1).contentId())
+                : null;
 
-        List<ContentDTO> result = contents.stream()
-                .map(content-> new ContentDTO(
-                        content.getId(),
-                        content.getTitle(),
-                        content.getPosterUrl(),
-                        content.getOpenDate(),
-                        content.getRating()
-                )).toList();
-
-        return new CursorPageResponse<>(result, nextCursor, hasNext);
+        return new CursorPageResponse<>(dtos, nextCursor, hasNext);
     }
 }
