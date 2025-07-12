d93a0b53e377d749732c2bbbc2ec152280a0b2d5
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-07-12 15:24:10 +0900

    UDT-21 test: ContentXXXFixture 도입

--- src/test/java/com/example/udtbe/admin/repository/AdminContentRepositoryTest.java
+++ src/test/java/com/example/udtbe/admin/repository/AdminContentRepositoryTest.java
@@ -4,11 +4,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import com.example.udtbe.common.fixture.ContentCastFixture;
+import com.example.udtbe.common.fixture.ContentCategoryFixture;
+import com.example.udtbe.common.fixture.ContentCountryFixture;
+import com.example.udtbe.common.fixture.ContentDirectorFixture;
 import com.example.udtbe.common.fixture.ContentFixture;
+import com.example.udtbe.common.fixture.ContentGenreFixture;
+import com.example.udtbe.common.fixture.ContentPlatformFixture;
 import com.example.udtbe.common.support.DataJpaSupport;
 import com.example.udtbe.domain.admin.dto.common.ContentDTO;
 import com.example.udtbe.domain.content.entity.Cast;
-import com.example.udtbe.domain.content.entity.Category;
 import com.example.udtbe.domain.content.entity.Content;
 import com.example.udtbe.domain.content.entity.ContentCast;
 import com.example.udtbe.domain.content.entity.ContentCategory;
@@ -17,12 +22,7 @@ import com.example.udtbe.domain.content.entity.ContentDirector;
 import com.example.udtbe.domain.content.entity.ContentGenre;
 import com.example.udtbe.domain.content.entity.ContentMetadata;
 import com.example.udtbe.domain.content.entity.ContentPlatform;
-import com.example.udtbe.domain.content.entity.Country;
-import com.example.udtbe.domain.content.entity.Director;
-import com.example.udtbe.domain.content.entity.Genre;
 import com.example.udtbe.domain.content.entity.Platform;
-import com.example.udtbe.domain.content.entity.enums.CategoryType;
-import com.example.udtbe.domain.content.entity.enums.GenreType;
 import com.example.udtbe.domain.content.entity.enums.PlatformType;
 import com.example.udtbe.domain.content.repository.ContentMetadataRepository;
 import com.example.udtbe.domain.content.repository.ContentRepository;
@@ -54,12 +54,15 @@ public class AdminContentRepositoryTest extends DataJpaSupport {
         Content content = ContentFixture.content("해리포터", "빅잼");
         // when
         Content save = contentRepository.save(content);
-        List<ContentCategory> contentCategories = defaultCategories(content);
-        List<ContentPlatform> contentPlatforms = defaultPlatforms(content, 3);
-        List<ContentCountry> contentCountries = defaultCountries(content, 3);
-        List<ContentGenre> contentGenres = defaultGenres(contentCategories, content);
-        List<ContentCast> contentCasts = defaultCasts(content, 30);
-        List<ContentDirector> contentDirectors = defaultDirectors(content, 3);
+        List<ContentCategory> contentCategories = ContentCategoryFixture.contentCategories(content);
+        List<ContentPlatform> contentPlatforms = ContentPlatformFixture.contentPlatforms(content,
+                3);
+        List<ContentCountry> contentCountries = ContentCountryFixture.contentCountries(content, 3);
+        List<ContentGenre> contentGenres = ContentGenreFixture.contentGenres(contentCategories,
+                content);
+        List<ContentCast> contentCasts = ContentCastFixture.contentCasts(content, 30);
+        List<ContentDirector> contentDirectors = ContentDirectorFixture.contentDirectors(content,
+                3);
 
         // then
         assertAll("연관관계까지 함께 저장 및 로딩",
@@ -155,73 +158,6 @@ public class AdminContentRepositoryTest extends DataJpaSupport {
             Cast cast = Cast.of("박연진" + i, "https://example.com/cast" + i);
             ContentCast contentCast = ContentCast.of(content, cast);
             list.add(contentCast);
-        });
-        return list;
-    }
-
-    private List<ContentDirector> defaultDirectors(Content content, int count) {
-        List<ContentDirector> list = new ArrayList<>();
-        IntStream.rangeClosed(1, count).forEach(i -> {
-            Director director = Director.of("감스트" + i);
-            ContentDirector contentDirector = ContentDirector.of(content, director);
-            list.add(contentDirector);
-        });
-        return list;
-    }
-
-    private List<ContentCountry> defaultCountries(Content content, int count) {
-        List<ContentCountry> list = new ArrayList<>();
-
-        IntStream.rangeClosed(1, count).forEach(i -> {
-            Country country = Country.of("대한민국" + i);
-            ContentCountry contentCountry = ContentCountry.of(content, country);
-            list.add(contentCountry);
-        });
-        return list;
-    }
-
-    private List<ContentCategory> defaultCategories(Content content) {
-        List<ContentCategory> list = new ArrayList<>();
-        Category category1 = Category.of(CategoryType.DRAMA);
-        Category category2 = Category.of(CategoryType.MOVIE);
-        Category category3 = Category.of(CategoryType.ANIMATION);
-        ContentCategory contentCategory1 = ContentCategory.of(content, category1);
-        ContentCategory contentCategory2 = ContentCategory.of(content, category2);
-        ContentCategory contentCategory3 = ContentCategory.of(content, category3);
-
-        list.add(contentCategory1);
-        list.add(contentCategory2);
-        list.add(contentCategory3);
-        return list;
-    }
-
-    private List<ContentGenre> defaultGenres(List<ContentCategory> contentCategories,
-            Content content) {
-        List<ContentGenre> list = new ArrayList<>();
-
-        contentCategories.forEach(c -> {
-            if (c.getCategory().getCategoryType().equals(CategoryType.DRAMA)) {
-                Genre genre1 = Genre.of(GenreType.DRAMA, c.getCategory());
-                Genre genre2 = Genre.of(GenreType.ROMANCE, c.getCategory());
-                ContentGenre contentGenre1 = ContentGenre.of(content, genre1);
-                ContentGenre contentGenre2 = ContentGenre.of(content, genre2);
-                list.add(contentGenre1);
-                list.add(contentGenre2);
-            } else if (c.getCategory().getCategoryType().equals(CategoryType.MOVIE)) {
-                Genre genre1 = Genre.of(GenreType.ACTION, c.getCategory());
-                Genre genre2 = Genre.of(GenreType.COMEDY, c.getCategory());
-                Genre genre3 = Genre.of(GenreType.ROMANCE, c.getCategory());
-                ContentGenre contentGenre1 = ContentGenre.of(content, genre1);
-                ContentGenre contentGenre2 = ContentGenre.of(content, genre2);
-                ContentGenre contentGenre3 = ContentGenre.of(content, genre3);
-                list.add(contentGenre1);
-                list.add(contentGenre2);
-                list.add(contentGenre3);
-            } else if (c.getCategory().getCategoryType().equals(CategoryType.ANIMATION)) {
-                Genre genre1 = Genre.of(GenreType.KIDS, c.getCategory());
-                ContentGenre contentGenre1 = ContentGenre.of(content, genre1);
-                list.add(contentGenre1);
-            }
         });
         return list;
     }
---
+++ src/test/java/com/example/udtbe/common/fixture/ContentCastFixture.java
@@ -0,0 +1,22 @@
+package com.example.udtbe.common.fixture;
+
+import com.example.udtbe.domain.content.entity.Cast;
+import com.example.udtbe.domain.content.entity.Content;
+import com.example.udtbe.domain.content.entity.ContentCast;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ContentCastFixture {
+
+    public static List<ContentCast> contentCasts(Content content, int count) {
+        List<ContentCast> list = new ArrayList<>();
+
+        IntStream.rangeClosed(1, count).forEach(i -> {
+            Cast cast = Cast.of("박연진" + i, "https://example.com/cast" + i);
+            ContentCast contentCast = ContentCast.of(content, cast);
+            list.add(contentCast);
+        });
+        return list;
+    }
+}
