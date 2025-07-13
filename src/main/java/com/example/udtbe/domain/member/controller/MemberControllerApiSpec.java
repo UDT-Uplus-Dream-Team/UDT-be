4cb22705caaad0cafc3c94be706c0b5265aa8e15
dnjstjt1297 <128368235+dnjstjt1297@users.noreply.github.com>
2025-07-14 08:22:55 +0900

    feat/UDT-63-회원-선호-장르-수정 (#30)
    * UDT-63 fix: Category 에러 -> Genre 에러
    
    * UDT-63 fix: 회원 선호 장르 수정 API
    
    * UDT-63 refactor: MemberRepository -> MemberRepositoryTest
    
    * UDT-63 test: Service 단위 테스트
    
    * UDT-63 test: Controller 통합 테스트
    
    * UDT-63 refactor: 사용하지 않는 메서드 삭제
    
    * UDT-63 test: Update Genre 단위 테스트 수정
    
    * UDT-63 feat: Vaild 추가
    
    * UDT-63 refactor: DisplayName 수정
    
    * UDT-63 test: Vaild 추가에 따른 Controller 테스트 추가

--- src/main/java/com/example/udtbe/domain/member/controller/MemberControllerApiSpec.java
+++ src/main/java/com/example/udtbe/domain/member/controller/MemberControllerApiSpec.java
@@ -1,13 +1,18 @@
 package com.example.udtbe.domain.member.controller;
 
+import com.example.udtbe.domain.member.dto.request.MemberUpdateGenreRequest;
 import com.example.udtbe.domain.member.dto.response.MemberInfoResponse;
+import com.example.udtbe.domain.member.dto.response.MemberUpdateGenreResponse;
 import com.example.udtbe.domain.member.entity.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "Member API", description = "회원 관련 API")
@@ -19,5 +24,13 @@ public interface MemberControllerApiSpec {
     @GetMapping("/users/me")
     ResponseEntity<MemberInfoResponse> getMemberInfo(
             @AuthenticationPrincipal Member member
+    );
+
+    @Operation(summary = "마이페이지에서 유저 선호 장르 수정")
+    @ApiResponse(useReturnTypeSchema = true)
+    @PatchMapping("/users/survey/genre")
+    ResponseEntity<MemberUpdateGenreResponse> updateSurveyGenres(
+            @AuthenticationPrincipal Member member,
+            @Valid @RequestBody MemberUpdateGenreRequest memberUpdateGenreRequest
     );
 }
