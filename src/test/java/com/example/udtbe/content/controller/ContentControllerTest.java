c92af589c66b3bdb7420f6310164fd88e198d09d
dudxo <father8475@naver.com>
2025-07-15 16:32:26 +0900

    UDT-82 test: OTT 콘텐츠 상세 정보 조회 Controller Integration Test

--- src/test/java/com/example/udtbe/content/controller/ContentControllerTest.java
+++ src/test/java/com/example/udtbe/content/controller/ContentControllerTest.java
@@ -1,39 +1,55 @@
 package com.example.udtbe.content.controller;
 
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.startsWith;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.example.udtbe.common.fixture.CastFixture;
 import com.example.udtbe.common.fixture.CategoryFixture;
+import com.example.udtbe.common.fixture.ContentCastFixture;
 import com.example.udtbe.common.fixture.ContentCategoryFixture;
 import com.example.udtbe.common.fixture.ContentCountryFixture;
+import com.example.udtbe.common.fixture.ContentDirectorFixture;
 import com.example.udtbe.common.fixture.ContentFixture;
 import com.example.udtbe.common.fixture.ContentGenreFixture;
 import com.example.udtbe.common.fixture.ContentPlatformFixture;
 import com.example.udtbe.common.fixture.CountryFixture;
+import com.example.udtbe.common.fixture.DirectorFixture;
 import com.example.udtbe.common.fixture.GenreFixture;
 import com.example.udtbe.common.fixture.PlatformFixture;
 import com.example.udtbe.common.support.ApiSupport;
 import com.example.udtbe.domain.content.controller.ContentController;
+import com.example.udtbe.domain.content.entity.Cast;
 import com.example.udtbe.domain.content.entity.Category;
 import com.example.udtbe.domain.content.entity.Content;
+import com.example.udtbe.domain.content.entity.ContentCast;
 import com.example.udtbe.domain.content.entity.ContentCategory;
 import com.example.udtbe.domain.content.entity.ContentCountry;
+import com.example.udtbe.domain.content.entity.ContentDirector;
 import com.example.udtbe.domain.content.entity.ContentGenre;
 import com.example.udtbe.domain.content.entity.ContentPlatform;
 import com.example.udtbe.domain.content.entity.Country;
+import com.example.udtbe.domain.content.entity.Director;
 import com.example.udtbe.domain.content.entity.Genre;
 import com.example.udtbe.domain.content.entity.Platform;
+import com.example.udtbe.domain.content.repository.CastRepository;
 import com.example.udtbe.domain.content.repository.CategoryRepository;
+import com.example.udtbe.domain.content.repository.ContentCastRepository;
 import com.example.udtbe.domain.content.repository.ContentCategoryRepository;
 import com.example.udtbe.domain.content.repository.ContentCountryRepository;
+import com.example.udtbe.domain.content.repository.ContentDirectorRepository;
 import com.example.udtbe.domain.content.repository.ContentGenreRepository;
 import com.example.udtbe.domain.content.repository.ContentPlatformRepository;
 import com.example.udtbe.domain.content.repository.ContentRepository;
 import com.example.udtbe.domain.content.repository.CountryRepository;
+import com.example.udtbe.domain.content.repository.DirectorRepository;
 import com.example.udtbe.domain.content.repository.GenreRepository;
 import com.example.udtbe.domain.content.repository.PlatformRepository;
 import java.time.LocalDateTime;
@@ -43,6 +59,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.util.ReflectionTestUtils;
 
 class ContentControllerTest extends ApiSupport {
 
@@ -75,6 +92,18 @@ class ContentControllerTest extends ApiSupport {
 
     @Autowired
     ContentGenreRepository contentGenreRepository;
+
+    @Autowired
+    CastRepository castRepository;
+
+    @Autowired
+    ContentCastRepository contentCastRepository;
+
+    @Autowired
+    DirectorRepository directorRepository;
+
+    @Autowired
+    ContentDirectorRepository contentDirectorRepository;
 
     @AfterEach
     void tearDown() {
@@ -309,6 +338,106 @@ class ContentControllerTest extends ApiSupport {
                 .andExpect(jsonPath("$.hasNext").value(true))
         ;
     }
+
+    @DisplayName("콘텐츠의 상세 정보를 조회한다.")
+    @Test
+    void getContentDetails() throws Exception {
+        // given
+        final String title = "세부 콘텐츠 조회";
+        final LocalDateTime openDate = LocalDateTime.of(2025, 7, 15, 14, 0);
+        final String rating = "전체 관람가";
+        Content savedContent = contentRepository.save(
+                ContentFixture.content(title, openDate, rating));
+
+        List<Platform> savedPlatforms = platformRepository.saveAll(PlatformFixture.platforms());
+        List<ContentPlatform> contentPlatforms = new ArrayList<>();
+        initContentPlatform(contentPlatforms, savedContent, savedPlatforms.get(3));
+        initContentPlatform(contentPlatforms, savedContent, savedPlatforms.get(6));
+        contentPlatformRepository.saveAll(contentPlatforms);
+
+        List<Cast> savedCasts = castRepository.saveAll(CastFixture.casts());
+        List<ContentCast> contentCasts = new ArrayList<>();
+        initContentCasts(contentCasts, savedContent, savedCasts.get(0));
+        initContentCasts(contentCasts, savedContent, savedCasts.get(1));
+        initContentCasts(contentCasts, savedContent, savedCasts.get(2));
+        contentCastRepository.saveAll(contentCasts);
+
+        List<Director> savedDirector = directorRepository.saveAll(DirectorFixture.directors());
+        List<ContentDirector> contentDirectors = new ArrayList<>();
+        initContentDirectors(contentDirectors, savedContent, savedDirector.get(3));
+        contentDirectorRepository.saveAll(contentDirectors);
+
+        List<Country> savedCountries = countryRepository.saveAll(CountryFixture.countries());
+        List<ContentCountry> contentCountries = new ArrayList<>();
+        initContentCountry(contentCountries, savedContent, savedCountries.get(0));
+        initContentCountry(contentCountries, savedContent, savedCountries.get(3));
+        contentCountryRepository.saveAll(contentCountries);
+
+        List<Category> savedCategories = categoryRepository.saveAll(CategoryFixture.categories());
+        List<ContentCategory> contentCategories = new ArrayList<>();
+        initContentCategory(contentCategories, savedContent, savedCategories.get(0));
+        contentCategoryRepository.saveAll(contentCategories);
+
+        List<Genre> savedGenres = genreRepository.saveAll(
+                GenreFixture.genres(savedCategories.get(0)));
+        List<ContentGenre> contentGenres = new ArrayList<>();
+        initContentGenre(contentGenres, savedContent, savedGenres.get(0));
+        initContentGenre(contentGenres, savedContent, savedGenres.get(1));
+        initContentGenre(contentGenres, savedContent, savedGenres.get(2));
+        contentGenreRepository.saveAll(contentGenres);
+
+        // when  // then
+        mockMvc.perform(get("/api/contents/{contentId}", savedContent.getId())
+                        .contentType(APPLICATION_JSON)
+                        .cookie(accessTokenOfMember)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.contentId").value(savedContent.getId()))
+                .andExpect(jsonPath("$.title").value(title))
+                .andExpect(jsonPath("$.openDate", startsWith(openDate.toString())))
+                .andExpect(jsonPath("$.rating").value(rating))
+                .andExpect(jsonPath("$.platforms[0].platformType").value("웨이브"))
+                .andExpect(jsonPath("$.platforms[1].platformType").value("Apple TV"))
+                .andExpect(jsonPath("$.casts[0].castName").value("마동석"))
+                .andExpect(jsonPath("$.casts[1].castName").value("황정민"))
+                .andExpect(jsonPath("$.casts[2].castName").value("토니스타크"))
+                .andExpect(jsonPath("$.directors", contains("김원석")))
+                .andExpect(jsonPath("$.countries", contains("한국", "미국")))
+                .andExpect(jsonPath("$.categories", contains("영화")))
+                .andExpect(jsonPath("$.genres", contains("액션", "판타지", "SF")))
+        ;
+    }
+
+    @DisplayName("존재하지 않은 콘텐츠를 조회할 수 없다.")
+    @Test
+    void throwExceptionWhenContentIsNotExist() throws Exception {
+
+        Content content = ContentFixture.content("존재하지 않는 콘텐츠", "존재하지 않는 콘텐츠입니다.");
+        ReflectionTestUtils.setField(content, "id", 100L);
+
+        // when  // then
+        mockMvc.perform(get("/api/contents/{contentId}", content.getId())
+                        .contentType(APPLICATION_JSON)
+                        .cookie(accessTokenOfMember)
+                )
+                .andDo(print())
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.code").value("CONTENT_NOT_FOUND"))
+                .andExpect(jsonPath("$.message").value("콘텐츠를 찾을 수 없습니다."))
+        ;
+    }
+
+    private void initContentDirectors(List<ContentDirector> contentDirectors, Content content,
+            Director director) {
+        contentDirectors.add(ContentDirectorFixture.contentDirector(content, director));
+    }
+
+    private void initContentCasts(List<ContentCast> contentCasts, Content content,
+            Cast cast) {
+        contentCasts.add(ContentCastFixture.contentCast(content, cast));
+    }
+
 
     private void initContent(List<Content> contents, String title, int year, String rating) {
         contents.add(ContentFixture.content(title, LocalDateTime.of(year, 1, 1, 0, 0), rating));
