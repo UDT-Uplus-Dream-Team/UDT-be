0d2b15660187a209515b2c3223dc8db2db39145f
dudxo <father8475@naver.com>
2025-07-17 11:34:40 +0900

    UDT-76 test: 설문조사 저장 로직 수정에 따른 Mocking추가

--- src/test/java/com/example/udtbe/survey/service/SurveyServiceTest.java
+++ src/test/java/com/example/udtbe/survey/service/SurveyServiceTest.java
@@ -4,6 +4,8 @@ import static com.example.udtbe.domain.member.entity.enums.Role.ROLE_GUEST;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -15,20 +17,25 @@ import com.example.udtbe.domain.survey.exception.SurveyErrorCode;
 import com.example.udtbe.domain.survey.service.SurveyQuery;
 import com.example.udtbe.domain.survey.service.SurveyService;
 import com.example.udtbe.global.exception.RestApiException;
+import com.example.udtbe.global.token.cookie.CookieUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockHttpServletResponse;
 
 @ExtendWith(MockitoExtension.class)
 class SurveyServiceTest {
 
     @Mock
     SurveyQuery surveyQuery;
+
+    @Mock
+    CookieUtil cookieUtil;
 
     @InjectMocks
     SurveyService surveyService;
@@ -38,17 +45,19 @@ class SurveyServiceTest {
     void createSurvey() {
         // given
         final String email = "test@naver.com";
+        final HttpServletResponse response = new MockHttpServletResponse();
 
         Member member = MemberFixture.member(email, ROLE_GUEST);
         List<String> platforms = List.of("넷플릭스", "디즈니+");
         List<String> genres = List.of("코미디", "범죄");
         SurveyCreateRequest request = new SurveyCreateRequest(platforms, genres);
 
-        BDDMockito.given(surveyQuery.existsByMember(member)).willReturn(Boolean.FALSE);
-        BDDMockito.given(surveyQuery.save(any(Survey.class))).willReturn(any(Survey.class));
+        given(surveyQuery.existsByMember(member)).willReturn(Boolean.FALSE);
+        given(surveyQuery.save(any(Survey.class))).willReturn(null);
+        willDoNothing().given(cookieUtil).deleteCookie(any(HttpServletResponse.class));
 
         // when
-        surveyService.createSurvey(request, member);
+        surveyService.createSurvey(request, member, response);
 
         // then
         assertAll(
@@ -62,16 +71,17 @@ class SurveyServiceTest {
     void throwExceptionIfSurveyAlreadyExists() {
         // given
         final String email = "test@naver.com";
+        final HttpServletResponse response = new MockHttpServletResponse();
 
         Member member = MemberFixture.member(email, ROLE_GUEST);
         List<String> platforms = List.of("넷플릭스", "디즈니+");
         List<String> genres = List.of("코미디", "범죄");
         SurveyCreateRequest request = new SurveyCreateRequest(platforms, genres);
 
-        BDDMockito.given(surveyQuery.existsByMember(member)).willReturn(Boolean.TRUE);
-
+        given(surveyQuery.existsByMember(member)).willReturn(Boolean.TRUE);
+        
         // when  // then
-        assertThatThrownBy(() -> surveyService.createSurvey(request, member))
+        assertThatThrownBy(() -> surveyService.createSurvey(request, member, response))
                 .isInstanceOf(RestApiException.class)
                 .hasMessage(SurveyErrorCode.SURVEY_ALREADY_EXISTS_FOR_MEMBER.getMessage());
     }
