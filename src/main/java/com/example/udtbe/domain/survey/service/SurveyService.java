d9c2fbe51b1feac9d2987651fbf58212e9e639bc
권영태 <56019823+dudxo@users.noreply.github.com>
2025-07-17 13:58:42 +0900

    feat/UDT-76-설문조사-후-회원-ROLE-업데이트 (#45)
    * UDT-76 feat: 설문조사 후 회원 역할 업데이트
    
    * UDT-76 test: 설문조사 저장 로직 수정에 따른 Mocking추가

--- src/main/java/com/example/udtbe/domain/survey/service/SurveyService.java
+++ src/main/java/com/example/udtbe/domain/survey/service/SurveyService.java
@@ -1,21 +1,29 @@
 package com.example.udtbe.domain.survey.service;
 
+import static com.example.udtbe.domain.member.entity.enums.Role.ROLE_USER;
+
 import com.example.udtbe.domain.member.entity.Member;
 import com.example.udtbe.domain.survey.dto.SurveyMapper;
 import com.example.udtbe.domain.survey.dto.request.SurveyCreateRequest;
 import com.example.udtbe.domain.survey.entity.Survey;
 import com.example.udtbe.domain.survey.exception.SurveyErrorCode;
 import com.example.udtbe.global.exception.RestApiException;
+import com.example.udtbe.global.token.cookie.CookieUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class SurveyService {
 
     private final SurveyQuery surveyQuery;
+    private final CookieUtil cookieUtil;
 
-    public void createSurvey(SurveyCreateRequest request, Member member) {
+    @Transactional
+    public void createSurvey(SurveyCreateRequest request, Member member,
+            HttpServletResponse response) {
         if (surveyQuery.existsByMember(member)) {
             throw new RestApiException(SurveyErrorCode.SURVEY_ALREADY_EXISTS_FOR_MEMBER);
         }
@@ -23,5 +31,7 @@ public class SurveyService {
         Survey survey = SurveyMapper.toEntity(request, member);
         surveyQuery.save(survey);
 
+        member.updateRole(ROLE_USER);
+        cookieUtil.deleteCookie(response);
     }
 }
