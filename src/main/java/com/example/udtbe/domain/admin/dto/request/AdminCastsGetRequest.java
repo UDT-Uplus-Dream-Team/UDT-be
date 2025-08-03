bcb2ef90172518967057c85c5f4aa4444a2aa464
dudxo <father8475@naver.com>
2025-08-03 20:09:20 +0900

    UDT-227 feat: 출연진/감독 조회 Req DTO CustomGetter Scheam hidden

--- src/main/java/com/example/udtbe/domain/admin/dto/request/AdminCastsGetRequest.java
+++ src/main/java/com/example/udtbe/domain/admin/dto/request/AdminCastsGetRequest.java
@@ -1,5 +1,6 @@
 package com.example.udtbe.domain.admin.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -14,6 +15,7 @@ public record AdminCastsGetRequest(
         Integer size
 ) {
 
+    @Schema(hidden = true)
     public int getSizeOrDefault() {
         return Objects.nonNull(size) ? size : 10;
     }
