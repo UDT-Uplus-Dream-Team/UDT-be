690bd65d790c49f69c01dd5964f12e4508ce2a4b
dnjstjt1297 <dnjstjt1297@gmail.com>
2025-08-06 06:54:38 +0900

    UDT-217 feat: 배치 작업 상세보기 RESPONSE 변경

--- src/main/java/com/example/udtbe/domain/admin/dto/response/AdminContentDelJobGetDetailResponse.java
+++ src/main/java/com/example/udtbe/domain/admin/dto/response/AdminContentDelJobGetDetailResponse.java
@@ -1,8 +1,12 @@
 package com.example.udtbe.domain.admin.dto.response;
 
+import com.example.udtbe.domain.batch.entity.enums.BatchStatus;
+
 public record AdminContentDelJobGetDetailResponse(
 
         Long batchJobMetricId,
+
+        BatchStatus status,
 
         Long contentId,
 
