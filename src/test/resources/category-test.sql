7047b33bd472cd2c9fce9115ce4800ff679d1861
dudxo <father8475@naver.com>
2025-10-16 01:35:30 +0900

    UDT-274 feat: Test Data SQL Script 추가

---
+++ src/test/resources/category-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO category (category_id, category_type, created_at, updated_at, is_deleted)
+VALUES (1, 'MOVIE', NOW(), NOW(), FALSE),
+       (2, 'DRAMA', NOW(), NOW(), FALSE),
+       (3, 'ANIMATION', NOW(), NOW(), FALSE),
+       (4, 'VARIETY', NOW(), NOW(), FALSE);
---
+++ src/test/resources/platform-test.sql
@@ -0,0 +1,8 @@
+INSERT INTO platform (platform_id, platform_type, created_at, updated_at, is_deleted)
+VALUES (1, 'NETFLIX', NOW(), NOW(), FALSE),
+       (2, 'TVING', NOW(), NOW(), FALSE),
+       (3, 'COUPANG_PLAY', NOW(), NOW(), FALSE),
+       (4, 'WAVVE', NOW(), NOW(), FALSE),
+       (5, 'DISNEY_PLUS', NOW(), NOW(), FALSE),
+       (6, 'WATCHA', NOW(), NOW(), FALSE),
+       (7, 'APPLE_TV', NOW(), NOW(), FALSE);
