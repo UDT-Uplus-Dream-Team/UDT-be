a11e981f74010c9a59c392f681642276189e93e6
hjg727 <tony9800@naver.com>
2025-11-01 15:10:34 +0900

    UDT-223 test: FeedbackFixture 시간 제어 기능 추가

--- src/test/java/com/example/udtbe/common/fixture/FeedbackFixture.java
+++ src/test/java/com/example/udtbe/common/fixture/FeedbackFixture.java
@@ -6,13 +6,25 @@ import com.example.udtbe.domain.content.entity.Content;
 import com.example.udtbe.domain.content.entity.Feedback;
 import com.example.udtbe.domain.content.entity.enums.FeedbackType;
 import com.example.udtbe.domain.member.entity.Member;
+import java.time.LocalDateTime;
 import lombok.NoArgsConstructor;
+import org.springframework.test.util.ReflectionTestUtils;
 
 @NoArgsConstructor(access = PRIVATE)
 public class FeedbackFixture {
 
     public static Feedback feedback(Member member, Content content, FeedbackType type) {
-        return Feedback.of(type, false, member, content);
+        Feedback feedback = Feedback.of(type, false, member, content);
+        ReflectionTestUtils.setField(feedback, "updatedAt", LocalDateTime.now());
+        return feedback;
+    }
+
+    public static Feedback feedbackWithTime(Member member, Content content, FeedbackType type,
+            LocalDateTime createdAt, LocalDateTime updatedAt) {
+        Feedback feedback = Feedback.of(type, false, member, content);
+        ReflectionTestUtils.setField(feedback, "createdAt", createdAt);
+        ReflectionTestUtils.setField(feedback, "updatedAt", updatedAt);
+        return feedback;
     }
 
     public static Feedback like(Member member, Content content) {
